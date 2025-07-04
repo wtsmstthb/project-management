de62deb62aa3f6bfd2176c39758e3abe68430d75
wtsmstthb <adamishak001@gmail.com>
2025-07-05 02:04:43 +0800

    reupdate again s3

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "pm-s3-images.s3.us-east-2.amazonaws.com",
+        hostname: "tpm-s3-images-for-adam.s3.ap-southeast-1.amazonaws.com",
         port: "",
         pathname: "/**",
       }
