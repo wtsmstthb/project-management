95fd81cb38a74e4a0d030df1f43476165dbf9263
wtsmstthb <adamishak001@gmail.com>
2025-07-05 18:15:50 +0800

    task

--- client/src/components/ModalNewTask/index.tsx
+++ client/src/components/ModalNewTask/index.tsx
@@ -47,8 +47,9 @@ const ModalNewTask = ({ isOpen, onClose, id = null }: Props) => {
   };
 
   const isFormValid = () => {
-    return title && authorUserId && !(id !== null || projectId);
-  };
+  return title && authorUserId && (id !== null || projectId);
+};
+
 
   const selectStyles =
     "mb-4 block w-full rounded border border-gray-300 px-3 py-2 dark:border-dark-tertiary dark:bg-dark-tertiary dark:text-white dark:focus:outline-none";
