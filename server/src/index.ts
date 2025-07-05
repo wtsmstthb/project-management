6a9067c8b61d7d27d7303d92dabe8596a1321993
wtsmstthb <adamishak001@gmail.com>
2025-07-05 16:15:48 +0800

    add user routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,6 +12,8 @@ import searchRoutes from "./routes/searchRoutes";
 import userRoutes from "./routes/userRoutes";
 import teamRoutes from "./routes/teamRoutes";
 
+
+
 /* CONFIGURATIONS */
 dotenv.config();
 const app = express();
@@ -33,6 +35,10 @@ app.use("/tasks", taskRoutes);
 app.use("/search", searchRoutes);
 app.use("/users", userRoutes);
 app.use("/teams", teamRoutes);
+
+
+
+
 
 /* SERVER */
 const port = Number(process.env.PORT) || 3001;
--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getUsers } from "../controllers/userController";
+import { getUsers,postUser } from "../controllers/userController";
 
 const router = Router();
 
 // ✅ This will respond to GET /users
 router.get("/", getUsers);
+router.post("/", postUser);
 
 export default router;
